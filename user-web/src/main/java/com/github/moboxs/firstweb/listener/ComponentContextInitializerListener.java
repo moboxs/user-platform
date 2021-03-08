215d80bd1d157111b41fab512f83e6a21b6de58f
moboxs <liuhh26@sina.com>
2021-03-08 22:05:06 +0800

    fix

--- user-web/src/main/java/com/github/moboxs/firstweb/listener/ComponentContextInitializerListener.java
+++ user-web/src/main/java/com/github/moboxs/firstweb/listener/ComponentContextInitializerListener.java
@@ -24,6 +24,7 @@ public class ComponentContextInitializerListener implements ServletContextListen
     public void contextDestroyed(ServletContextEvent sce) {
         ComponentContext componentContext = ComponentContext.getInstance();
         componentContext.destroy();
+
     }
 
 }
