b105a36dcdcad40659ac9b5311af42cfa3dbcf67
moboxs <liuhh26@sina.com>
2021-03-27 19:07:54 +0800

    fix

--- user-web/src/test/java/com/github/moboxs/httpclient/Client.java
+++ user-web/src/test/java/com/github/moboxs/httpclient/Client.java
@@ -1,5 +1,21 @@
 package com.github.moboxs.httpclient;
 
 public class Client {
+    public static void main(String[] args) {
+        Integer aa = 127;
+        Integer bb = 127;
+        System.out.println(aa == bb);
+        aa = 129;
+        bb = 129;
 
+        System.out.println(aa > bb);
+
+    }
+
+    public <T> T com() {
+
+        T t = (T) Integer.valueOf(1);
+
+        return t;
+    }
 }
